8a1026bfac0d0050f26587fa4efcf6b56b2c2814
Marion Lambert <marionlambert85@googlemail.com>
2021-01-04 18:12:49 +0100

    Add shopping list model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_04_162604) do
+ActiveRecord::Schema.define(version: 2021_01_04_165514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,14 @@ ActiveRecord::Schema.define(version: 2021_01_04_162604) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "shopping_lists", force: :cascade do |t|
+    t.bigint "dose_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["dose_id"], name: "index_shopping_lists_on_dose_id"
+  end
+
   add_foreign_key "doses", "ingredients"
   add_foreign_key "doses", "recipes"
+  add_foreign_key "shopping_lists", "doses"
 end
